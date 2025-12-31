a1dfde5133ed0ed11e73fb5117621156d3ce59b8
amishijain@123 <jainamishi2005@gmail.com>
2025-12-31 17:01:33 +0530

    Revert "chore: resolve merge conflicts and fix pubspec duplicate"
    This reverts commit cf230ff4bc20db5c970a9009918844921027f7af.

--- mobile-app/windows/flutter/generated_plugins.cmake
+++ mobile-app/windows/flutter/generated_plugins.cmake
@@ -5,10 +5,10 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_windows
   file_selector_windows
+  share_plus
   flutter_inappwebview_windows
   permission_handler_windows
   record_windows
-  share_plus
   url_launcher_windows
 )
 
